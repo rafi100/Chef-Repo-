4535c8a4d06ded45d9e4ae1b8987c924cb4a09f6
rafi3 <rafi3linux@gmail.com>
2018-05-06 09:41:17 +0530

    add app cookbook

--- cookbooks/app/recipes/default.rb
+++ cookbooks/app/recipes/default.rb
@@ -3,3 +3,21 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+package 'httpd' do
+action :install
+end
+
+service 'httpd' do 
+  action [:enable, :start]
+ end
+file '/home/ec2-user/shell.sh' do
+  action :create
+ end
+file '/home/ec2-user/shell1.sh' do
+  action :create
+ end
+ 
+#	execute 'httpd' do
+#	command 'systemctl stop httpd'
+#	action :run
+#end
